d515c7eebd6376e5909f0935644843337083d87f
Maria Ilieva <mila234@gmail.com>
2022-11-18 16:47:09 +0200

    Adding custom tag

--- scholar_system/main/templatetags/custom_tag.py
+++ scholar_system/main/templatetags/custom_tag.py
@@ -6,3 +6,9 @@ register = template.Library()
 @register.filter('has_permission')
 def has_permission(user, group):
     return user.groups.filter(group).exists()
+
+
+@register.filter('show_less')
+def show_less(text):
+    if len(text) > 300:
+        return text[:300]
