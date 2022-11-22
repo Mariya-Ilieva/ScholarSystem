b6ae9cefc85b9cfa965e09269e0504b84ec3d864
Maria Ilieva <mila234@gmail.com>
2022-11-22 15:07:09 +0200

    Fixing footer showing

--- scholar_system/seminars/models.py
+++ scholar_system/seminars/models.py
@@ -3,7 +3,7 @@ from scholar_system.seminars.validators import validate_future_date
 
 
 class Seminar(models.Model):
-    theme = models.CharField(max_length=35)
+    theme = models.CharField(max_length=100)
     date = models.DateField(validators=[validate_future_date, ])
     time = models.TimeField()
     link = models.URLField(max_length=200)
