49e2a322d373fb0f0d263a203327d12e3c80c53c
Maria Ilieva <mila234@gmail.com>
2022-11-23 17:56:43 +0200

    Fixing seminar date

--- scholar_system/seminars/signals.py
+++ scholar_system/seminars/signals.py
@@ -7,7 +7,7 @@ from scholar_system.seminars.models import Seminar
 @receiver(post_save, sender=Seminar)
 def delete_after_passed(sender, instance, created, **kwargs):
     if created:
-        while instance.date > date.today():
+        while instance.date >= date.today():
             pass
         else:
             instance.delete()
--- scholar_system/seminars/validators.py
+++ scholar_system/seminars/validators.py
@@ -3,6 +3,5 @@ from django.core.exceptions import ValidationError
 
 
 def validate_future_date(value):
-    today = date.today()
-    if value < today:
+    if value < date.today():
         raise ValidationError('Please enter a valid date for the seminar.')
