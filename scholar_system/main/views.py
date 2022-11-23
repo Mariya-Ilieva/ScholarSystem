8ff3ce0b7dbe93a4ce9aa44b484feb6cc0d151f3
Maria Ilieva <mila234@gmail.com>
2022-11-23 15:20:26 +0200

    Fixing accounts

--- scholar_system/main/views.py
+++ scholar_system/main/views.py
@@ -42,7 +42,6 @@ class AllPapersView(generic.ListView):
 class AllTopicPapersView(generic.ListView):
     model = Paper
     template_name = 'main/topic_papers.html'
-    paginate_by = 5
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
