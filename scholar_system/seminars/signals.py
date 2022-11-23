67726d3680b55c09ab46868905baa9f08f9edcb5
Maria Ilieva <mila234@gmail.com>
2022-11-23 15:44:48 +0200

    Adding signal for expired seminars

---
+++ scholar_system/seminars/signals.py
@@ -0,0 +1,13 @@
+from datetime import date
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from scholar_system.seminars.models import Seminar
+
+
+@receiver(post_save, sender=Seminar)
+def delete_after_passed(sender, instance, created, **kwargs):
+    if created:
+        while instance.date > date.today():
+            pass
+        else:
+            instance.delete()
