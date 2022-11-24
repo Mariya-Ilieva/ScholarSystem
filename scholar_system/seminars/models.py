9576a9bca6f4174b24d34076b31cb44ef5a51dbc
Maria Ilieva <mila234@gmail.com>
2022-11-24 19:40:52 +0200

    Adding remaining days to Seminar model

--- scholar_system/seminars/models.py
+++ scholar_system/seminars/models.py
@@ -1,3 +1,4 @@
+from datetime import date
 from django.db import models
 from scholar_system.seminars.validators import validate_future_date
 
@@ -7,3 +8,9 @@ class Seminar(models.Model):
     date = models.DateField(validators=[validate_future_date, ])
     time = models.TimeField()
     link = models.URLField(max_length=200)
+
+    @property
+    def days_till(self):
+        today = date.today()
+        days_till = str(self.date - today).split(',')[0]
+        return days_till
