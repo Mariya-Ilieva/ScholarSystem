8c07db3c7de7470ddbfaa27f178c69a64f888a8b
Maria Ilieva <mila234@gmail.com>
2022-11-30 14:05:14 +0200

    Creating users_API app

--- scholar_system/users_API/urls.py
+++ scholar_system/users_API/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from scholar_system.users_API.views import AllUsersAPIView, DetailsUserAPIView
+from scholar_system.users_API.views import UserListCreateView, UserUpdateDestroyView
 
 urlpatterns = [
-    path('', AllUsersAPIView.as_view(), name='users'),
-    path('<int:id>/', DetailsUserAPIView.as_view()),
+    path('', UserListCreateView.as_view(), name='users'),
+    path('<int:pk>/', UserUpdateDestroyView.as_view()),
 ]
--- scholar_system/users_API/views.py
+++ scholar_system/users_API/views.py
@@ -1,37 +1,46 @@
-from rest_framework import views, response, status
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from scholar_system.accounts.models import MasterUser
 from scholar_system.users_API.serializers import MasterUserSerializer, DetailMasterUserSerializer
 
 
-class AllUsersAPIView(views.APIView):
-    def get(self, request):
-        users = MasterUser.objects.all().order_by('id')
-        serializer = MasterUserSerializer(users, many=True)
-        return response.Response({'all_users': users.count(), 'users': serializer.data})
+# class AllUsersAPIView(views.APIView):
+#     def get(self, request):
+#         users = MasterUser.objects.all().order_by('id')
+#         serializer = MasterUserSerializer(users, many=True)
+#         return response.Response({'all_users': users.count(), 'users': serializer.data})
+#
+#     def post(self, request):
+#         serializer = MasterUserSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return response.Response(serializer.data, status.HTTP_201_CREATED)
+#         return response.Response(serializer.errors, status.HTTP_400_BAD_REQUEST)
+#
+#
+# class DetailsUserAPIView(views.APIView):
+#     def get(self, request, id):
+#         user = MasterUser.objects.get(pk=id)
+#         serializer = DetailMasterUserSerializer(user)
+#         return response.Response({'user': serializer.data})
+#
+#     def post(self, request, id):
+#         user = MasterUser.objects.get(pk=id)
+#         serializer = DetailMasterUserSerializer(user, request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return response.Response(serializer.data, status.HTTP_201_CREATED)
+#         return response.Response(serializer.errors, status.HTTP_400_BAD_REQUEST)
+#
+#     def delete(self, request, id):
+#         user = MasterUser.objects.get(pk=id)
+#         user.delete()
+#         return response.Response(status.HTTP_200_OK)
 
-    def post(self, request):
-        serializer = MasterUserSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return response.Response(serializer.data, status.HTTP_201_CREATED)
-        return response.Response(serializer.errors, status.HTTP_400_BAD_REQUEST)
+class UserListCreateView(ListCreateAPIView):
+    serializer_class = MasterUserSerializer
+    queryset = MasterUser.objects.all()
 
 
-class DetailsUserAPIView(views.APIView):
-    def get(self, request, id):
-        user = MasterUser.objects.get(pk=id)
-        serializer = DetailMasterUserSerializer(user)
-        return response.Response({'user': serializer.data})
-
-    def post(self, request, id):
-        user = MasterUser.objects.get(pk=id)
-        serializer = DetailMasterUserSerializer(user, request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return response.Response(serializer.data, status.HTTP_201_CREATED)
-        return response.Response(serializer.errors, status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, id):
-        user = MasterUser.objects.get(pk=id)
-        user.delete()
-        return response.Response(status.HTTP_200_OK)
+class UserUpdateDestroyView(RetrieveUpdateDestroyAPIView):
+    serializer_class = DetailMasterUserSerializer
+    queryset = MasterUser.objects.all()
