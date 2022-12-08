51af1ed8a43721365772333980d9a7d2694c21c7
Maria Ilieva <mila234@gmail.com>
2022-12-08 17:54:43 +0200

    Improvements

--- scholar_system/main/views.py
+++ scholar_system/main/views.py
@@ -13,19 +13,19 @@ def unauthorized(request):
 
 
 def bad_request(request, exception=None):
-    return render(request, 'main/bad_request.html')
+    return render(request, 'main/bad_request.html', status=400)
 
 
 def permission_denied(request, exception=None):
-    return render(request, 'main/permission_denied.html')
+    return render(request, 'main/permission_denied.html', status=403)
 
 
 def page_not_found(request, exception=None):
-    return render(request, 'main/not_found.html')
+    return render(request, 'main/not_found.html', status=404)
 
 
 def server_error(request, exception=None):
-    return render(request, 'main/server_error.html')
+    return render(request, 'main/server_error.html', status=500)
 
 
 class AllPapersView(ListView):
