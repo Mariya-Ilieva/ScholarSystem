6271dda3206f4f0633d70880742a921b39c0cfc3
Maria Ilieva <mila234@gmail.com>
2022-12-10 19:12:26 +0200

    Testing

--- scholar_system/papers/tests.py
+++ scholar_system/papers/tests.py
@@ -1,3 +1,25 @@
+from datetime import date
 from django.test import TestCase
+from scholar_system.accounts.models import Profile
+from scholar_system.papers.models import Paper
 
-# Create your tests here.
+
+class PaperTest(TestCase):
+    def setUp(self):
+        self.user = Profile.objects.create(username='test', age=22, first_name='Test', last_name='Testme', user_id=6)
+        self.user.save()
+        self.publication_date = date.today()
+        self.paper = Paper(created_by=self.user, description='test description', topic_id=6)
+        self.paper.save()
+
+    def tearDown(self):
+        self.user.delete()
+
+    def test_read_paper(self):
+        self.assertEqual(self.paper.created_by, self.user)
+        self.assertEqual(self.paper.description, 'test description')
+
+    def test_update_paper_description(self):
+        self.paper.description = 'updated test description'
+        self.paper.save()
+        self.assertEqual(self.paper.description, 'updated test description')
