46aeff330f9d2c663f0826a47eb073c989a61af2
Maria Ilieva <mila234@gmail.com>
2022-12-15 16:53:48 +0200

    Refactoring

--- scholar_system/settings.py
+++ scholar_system/settings.py
@@ -121,3 +121,42 @@ STAR_RATINGS_RANGE = 8
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# LOGGING = {
+#     'version': 1,
+#     'disable_existing_loggers': False,
+#     'formatters': {
+#         'verbose': {
+#             'format': '{levelname} {asctime} {module} {process:d} {thread:d} {message}',
+#             'style': '{',
+#         },
+#         'simple': {
+#             'format': '{levelname} {message}',
+#             'style': '{',
+#         },
+#     },
+#     'filters': {
+#         'require_debug_true': {
+#             '()': 'django.utils.log.RequireDebugTrue',
+#         },
+#     },
+#     'handlers': {
+#         'console': {
+#             'level': 'INFO',
+#             'filters': ['require_debug_true'],
+#             'class': 'logging.StreamHandler',
+#             'formatter': 'verbose'
+#         },
+#     },
+#     'loggers': {
+#         'django': {
+#             'handlers': ['console'],
+#             'propagate': True,
+#         },
+#         'scholar_system.custom': {
+#             'handlers': ['console'],
+#             'level': 'INFO',
+#             'filters': ['require_debug_true'],
+#         }
+#     }
+# }
